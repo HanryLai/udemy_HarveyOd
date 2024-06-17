474ebe2c70dd513830a528cd53d72f1da0c040d0
daenv <daenvle69@gmail.com>
2024-06-17 20:00:16 +0700

    chore: Refactor OtpService to use MessageResponse for OTP validation

--- src/apps/auth/otp/otp.service.ts
+++ src/apps/auth/otp/otp.service.ts
@@ -2,19 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { RedisService } from 'src/common/redis/redis.service';
 
 import { generateOtp } from 'src/utils';
+import { MessageResponse } from '../../../common';
 
 @Injectable()
 export class OtpService {
-   constructor(private redisService: RedisService) {}
+   constructor(private redisService: RedisService) {
+   }
+
    public async generateAndStoreOtp(email: string): Promise<string> {
       const otp = generateOtp();
       await this.redisService.set(email, otp, 180);
       return otp;
    }
 
-   public async validateOtp(email: string, otp: string): Promise<any> {
+   public async validateOtp(email: string, otp: string): Promise<MessageResponse> {
       const cacheOtp = await this.redisService.get(email);
-      return cacheOtp === otp;
+      if (!cacheOtp || cacheOtp !== otp) {
+         return {
+            success: false,
+            message: 'OTP is invalid',
+            data: {},
+         };
+      }
+
+      return {
+         success: true,
+         message: 'OTP is valid',
+         data: {},
+      };
    }
 
    public async deleteOtp(email: string): Promise<void> {
