99c3f14e92e8b61d3a2ac1f49924b3790a860e29
hieulai <ldmhieudev@gmail.com>
2024-06-25 21:53:22 +0700

    feat: Dto create course

--- src/apps/course/dto/create-course.dto.ts
+++ src/apps/course/dto/create-course.dto.ts
@@ -1,1 +1,42 @@
-export class CreateCourseDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Level } from '../enum/level.enum';
+
+export class CreateCourseDto {
+   @ApiProperty()
+   @IsString()
+   @IsNotEmpty()
+   title: string;
+
+   @ApiProperty()
+   @IsString()
+   @IsNotEmpty()
+   description: string;
+
+   @ApiProperty()
+   @IsArray()
+   @IsNotEmpty()
+   language: string[];
+
+   @ApiProperty()
+   @IsNumber({}, { message: 'Price must be a numeric string' })
+   price: number;
+
+   @ApiProperty()
+   @IsEnum(Level)
+   @IsNotEmpty()
+   level: Level;
+
+   @ApiProperty()
+   discount: number;
+
+   @ApiProperty()
+   @IsString()
+   @IsNotEmpty()
+   category: string;
+
+   @ApiProperty()
+   @IsString()
+   @IsNotEmpty()
+   thunbnailUrl: string;
+}
