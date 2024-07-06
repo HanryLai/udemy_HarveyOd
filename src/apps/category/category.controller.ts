b163640858a5c319b136438fd3e25de25e1429e5
hieulai <ldmhieudev@gmail.com>
2024-07-06 11:35:03 +0700

    Feat:Find category by id

--- src/apps/category/category.controller.ts
+++ src/apps/category/category.controller.ts
@@ -15,7 +15,7 @@ import {
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { MessageResponse } from 'src/common';
 import { CreateCourseDto } from '../course/dto/create-course.dto';
 import { ExistToken } from 'src/common/guards/exist-token.guard';
@@ -27,6 +27,15 @@ import { RequestInterceptor } from 'src/common/interceptors/token-current.interc
 export class CategoryController {
    constructor(private readonly categoryService: CategoryService) {}
 
+   @HttpCode(HttpStatus.FOUND)
+   @Get(':id')
+   @ApiOperation({ summary: 'Find category by id category' })
+   @ApiFoundResponse({ description: 'Find category by id category successfully' })
+   @ApiBody({ type: CreateCategoryDto, description: 'About scheme category' })
+   public async findById(@Param('id') id: string): Promise<MessageResponse> {
+      return await this.categoryService.findById(id);
+   }
+
    @HttpCode(HttpStatus.OK)
    @UseInterceptors(RequestInterceptor)
    @UseGuards(ExistToken)
@@ -34,7 +43,7 @@ export class CategoryController {
    @ApiOperation({ summary: 'Create new category' })
    @ApiOkResponse({ description: 'Create new category successfully' })
    @ApiBody({ type: CreateCourseDto, description: 'About schema category' })
-   public async createCategory(
+   public async create(
       @Body() category: CreateCategoryDto,
       @TokenCurrent() authToken: string,
    ): Promise<MessageResponse> {
