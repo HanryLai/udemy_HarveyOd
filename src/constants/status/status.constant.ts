4bbbe7618914afa6b69b0d738a7a118e589eb4c5
erichoanganle11 <erichoanganle@gmail.com>
2024-07-09 09:24:20 +0700

    refactor: Update status constants and responses for API handling

---
+++ src/constants/status/status.constant.ts
@@ -0,0 +1,32 @@
+export const STATUS_CODE: { [key: string]: number } = {
+   OK: 200,
+   CREATED: 201,
+   ACCEPTED: 202,
+   NO_CONTENT: 204,
+   BAD_REQUEST: 400,
+   UNAUTHORIZED: 401,
+   FORBIDDEN: 403,
+   NOT_FOUND: 404,
+   CONFLICT: 409,
+   INTERNAL_SERVER_ERROR: 500,
+   NOT_IMPLEMENTED: 501,
+   BAD_GATEWAY: 502,
+   SERVICE_UNAVAILABLE: 503,
+   GATEWAY_TIMEOUT: 504,
+};
+
+export const STATUS_REASON: { [key: number]: string } = {
+   200: 'OK',
+   201: 'Created',
+   202: 'Accepted',
+   204: 'No Content',
+   400: 'Bad Request',
+   401: 'Unauthorized',
+   403: 'Forbidden',
+   404: 'Not Found',
+   500: 'Internal Server Error',
+   501: 'Not Implemented',
+   502: 'Bad Gateway',
+   503: 'Service Unavailable',
+   504: 'Gateway Timeout',
+};
