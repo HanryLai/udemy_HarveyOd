fd3922e7708c5f9ef7fbb3b3b07d2a6839a78646
erichoanganle11 <erichoanganle@gmail.com>
2024-07-09 23:41:21 +0700

    refactor: Update otp.service.spec.ts to use new response classes

--- src/apps/auth/otp/otp.service.spec.ts
+++ src/apps/auth/otp/otp.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OtpService } from './otp.service';
 import { RedisService } from 'src/common/redis/redis.service';
+import { ErrorResponse, OK } from 'src/common';
 
 describe('OtpService', () => {
    let service: OtpService;
@@ -27,52 +28,40 @@ describe('OtpService', () => {
       redisService = module.get<RedisService>(RedisService);
    });
 
-   // it('should validate OTP', async () => {
-   //    const email = 'test@example.com';
-   //    const otp = '123456';
+   it('should validate OTP', async () => {
+      const email = 'test@example.com';
+      const otp = '123456';
 
-   //    jest.spyOn(redisService, 'get').mockImplementation(() => Promise.resolve(otp));
+      jest.spyOn(redisService, 'get').mockImplementation(() => Promise.resolve(otp));
 
-   //    const result = await service.validateOtp(email, otp);
+      const result = await service.validateOtp(email, otp);
 
-   //    expect(result).toStrictEqual({
-   //       success: true,
-   //       message: 'OTP is valid',
-   //       data: {},
-   //    });
-   //    expect(redisService.get).toHaveBeenCalledWith(email);
-   // });
+      expect(result).toStrictEqual(
+         new OK({
+            message: 'Otp is valid',
+            metadata: {},
+         }),
+      );
+      expect(redisService.get).toHaveBeenCalledWith(email);
+   });
 
-   // it('should not validate OTP if it does not match', async () => {
-   //    const email = 'test@example.com';
-   //    const otp = '123456';
-   //    const wrongOtp = '654321';
+   it('should not validate OTP if it does not match', async () => {
+      const email = 'test@example.com';
+      const otp = '123456';
+      const wrongOtp = '654321';
 
-   //    jest.spyOn(redisService, 'get').mockImplementation(() => Promise.resolve(otp));
+      jest.spyOn(redisService, 'get').mockImplementation(() => Promise.resolve(otp));
 
-   //    const result = await service.validateOtp(email, wrongOtp);
+      const result = await service.validateOtp(email, wrongOtp);
 
-   //    expect(result).toStrictEqual({
-   //       success: false,
-   //       message: 'OTP is invalid',
-   //       data: {},
-   //    });
-   //    expect(redisService.get).toHaveBeenCalledWith(email);
-   // });
-
-   // describe('generateAndStoreOtp', () => {
-   //    it('should generate an OTP, store it in Redis, and return it', async () => {
-   //       const email = 'test@test.com';
-   //       const otp = '123456';
-
-   //       const setSpy = jest.spyOn(redisService, 'set').mockImplementation(() => Promise.resolve());
-
-   //       const result = await service.generateAndStoreOtp(email);
-
-   //       expect(setSpy).toHaveBeenCalledWith(email, otp, 180);
-   //       expect(result).toBe(otp);
-   //    });
-   // });
+      expect(result).toStrictEqual(
+         new ErrorResponse({
+            message: 'OTP is invalid',
+            metadata: {},
+         }),
+      );
+      expect(redisService.get).toHaveBeenCalledWith(email);
+   });
 
    describe('deleteOtp', () => {
       it('should delete the OTP associated with the given email', async () => {
