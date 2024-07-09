072651753b05fe6fbde83c7ffc697c5d7e357da6
erichoanganle11 <erichoanganle@gmail.com>
2024-07-09 09:23:05 +0700

    feat: Update MailService to include OK response for sending OTP

--- src/apps/auth/mail/mail.service.ts
+++ src/apps/auth/mail/mail.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { Queue } from 'bull';
 
 import { MAIL_QUEUE, SEND_OTP_EMAIL_JOB } from 'src/constants';
-import { MessageResponse } from 'src/common';
+import { MessageResponse, OK } from 'src/common';
 import { ValidOtp } from './dto/vaild-otp.dto';
 import { SendOtpDto } from './dto/send-otp.dto';
 import { OtpService } from '../otp/otp.service';
@@ -13,8 +13,7 @@ export class MailService {
    constructor(
       @InjectQueue(MAIL_QUEUE) private mailQueue: Queue,
       private readonly otpService: OtpService,
-   ) {
-   }
+   ) {}
 
    public async sendOTPEmail(name: string, email: string, otp: string): Promise<boolean> {
       await this.mailQueue.add(SEND_OTP_EMAIL_JOB, { name, email, otp });
@@ -22,20 +21,19 @@ export class MailService {
    }
 
    public validateOtp(validOtp: ValidOtp): Promise<MessageResponse> {
-      return  this.otpService.validateOtp(validOtp.email, validOtp.otp);
+      return this.otpService.validateOtp(validOtp.email, validOtp.otp);
    }
 
    public async sendNewOtp(sendMailDto: SendOtpDto): Promise<MessageResponse> {
       const otp = await this.otpService.generateAndStoreOtp(sendMailDto.email);
       await this.sendOTPEmail(sendMailDto.username, sendMailDto.email, otp);
 
-      return {
-         success: true,
-         message: 'New OTP has been sent',
-         data: {
+      return new OK({
+         message: 'OTP sent successfully',
+         metadata: {
             email: sendMailDto.email,
             otp,
          },
-      };
+      });
    }
 }
