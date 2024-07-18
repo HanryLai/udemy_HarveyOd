2bb5e7f0dfd98a9fcbc159749eafa51ee9a3cf33
hieulai <ldmhieudev@gmail.com>
2024-07-18 18:56:43 +0700

    Feat: Dto for tag

--- src/apps/tag/dto/update-tag.dto.ts
+++ src/apps/tag/dto/update-tag.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateTagDto } from './create-tag.dto';
+import { IsString } from 'class-validator';
 
-export class UpdateTagDto extends PartialType(CreateTagDto) {}
+export class UpdateTagDto extends PartialType(CreateTagDto) {
+   @ApiProperty()
+   @IsString()
+   name: string;
+
+   @ApiProperty()
+   @IsString()
+   description: string;
+}
