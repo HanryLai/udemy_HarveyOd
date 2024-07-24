92dd45321280dc7955f6ece2c8932ae7c85a8b08
hieulai <ldmhieudev@gmail.com>
2024-07-24 21:14:28 +0700

    Feat: update spell lessonCourse entity

---
+++ src/entities/courses/typeUpload.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, OneToMany, OneToOne } from 'typeorm';
+import { BaseEntity } from '../bases';
+import { ITypeUploadEntity } from '../interfaces';
+import { CourseLessonEntity } from './lesson.entity';
+
+@Entity({ name: 'type_upload' })
+export class TypeUploadEntity extends BaseEntity<TypeUploadEntity> implements ITypeUploadEntity {
+   @Column({ array: true, type: 'text', name: 'type' })
+   type: string[];
+   @Column({ type: 'varchar', name: 'description' })
+   description: string;
+   @Column({ type: 'varchar', name: 'url' })
+   url: string;
+   @Column({ type: 'int', name: 'duration' })
+   duration: number;
+
+   @OneToMany(() => CourseLessonEntity, (lesson) => lesson.typeUpload)
+   lessons: CourseLessonEntity[];
+}
