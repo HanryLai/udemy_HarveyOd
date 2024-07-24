f1ea13828a8c6f7c9a536e53cb2f5263bcd97d28
hieulai <ldmhieudev@gmail.com>
2024-07-24 20:47:56 +0700

    Feat: add lessonEntity to database

---
+++ src/entities/courses/lessonTest.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, Index, ManyToOne } from 'typeorm';
+import { BaseEntity } from '../bases';
+import { ILessonTestEntity } from '../interfaces';
+import { CourseLessonEntity } from './lesson.entity';
+
+@Entity({ name: 'lession_test' })
+export class LessonTestEntity extends BaseEntity<LessonTestEntity> implements ILessonTestEntity {
+   @Index('IDX_LESSON_TEST')
+   @Column({ type: 'varchar', name: 'question_text' })
+   questionText: string;
+
+   @Column({ array: true, type: 'text', name: 'options' })
+   options: string[];
+
+   @Column({ type: 'varchar', name: 'correct_option' })
+   correctOption: string;
+   @Column({ type: 'varchar', name: 'url_audio' })
+   urlAudio: string;
+
+   @ManyToOne(() => CourseLessonEntity, (lesson) => lesson.lessonTest)
+   lessons: CourseLessonEntity;
+}
