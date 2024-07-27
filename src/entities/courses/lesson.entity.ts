907f07a6db17ad9070616d4543e3b60f1dac6553
erichoanganle11 <erichoanganle@gmail.com>
2024-07-27 19:06:09 +0700

    refactor: Update lesson.entity.ts for consistency and clarity

--- src/entities/courses/lesson.entity.ts
+++ src/entities/courses/lesson.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, Index, ManyToOne } from 'typeorm';
+import { Column, Entity, Index, JoinColumn, ManyToOne, OneToOne } from 'typeorm';
 import { BaseEntity } from '../bases';
 import { ILesson } from '../interfaces';
 import { CourseModuleEntity } from './module.entity';
@@ -7,26 +7,27 @@ import { LessionTestEntity } from './lessionTest.entity';
 
 @Entity({ name: 'course_lesson' })
 export class CourseLessonEntity extends BaseEntity<CourseLessonEntity> implements ILesson {
-   @Index('IDX_COURSE_LESSION')
-   @Column({ type: 'varchar', name: 'lession_title' })
+   @Index('IDX_COURSE_LESSON')
+   @Column({ type: 'varchar', name: 'lesson_title' })
    title: string;
 
-   @Column({ type: 'varchar', name: 'lession_content' })
+   @Column({ type: 'varchar', name: 'lesson_content' })
    content: string;
 
-   @Column({ type: 'varchar', name: 'lession_video_url' })
+   @Column({ type: 'varchar', name: 'lesson_video_url' })
    videoUrl: string;
 
-   @Column({ type: 'varchar', name: 'lession_duration' })
+   @Column({ type: 'varchar', name: 'lesson_duration' })
    duration: string;
 
-   @Column({ type: 'int', name: 'lession_order_index' })
+   @Column({ type: 'int', name: 'lesson_order_index' })
    orderIndex: number;
 
    @ManyToOne(() => CourseModuleEntity, (module) => module.lessions)
    module: CourseModuleEntity;
 
-   @ManyToOne(() => TypeUploadEntity, (typeUpload) => typeUpload.lesson)
+   @OneToOne(() => TypeUploadEntity, { cascade: true })
+   @JoinColumn()
    typeUpload: TypeUploadEntity;
 
    @ManyToOne(() => LessionTestEntity, (lesson) => lesson.lessons)
