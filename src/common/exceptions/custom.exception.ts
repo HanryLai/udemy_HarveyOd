620ec538104c0e3062ec2607e5d0f3206a66cabe
erichoanganle11 <erichoanganle@gmail.com>
2024-07-27 19:05:52 +0700

    refactor: Add logger to ErrorResponse class

--- src/common/exceptions/custom.exception.ts
+++ src/common/exceptions/custom.exception.ts
@@ -1,9 +1,11 @@
 import { Response } from 'express';
 import { MessageResponse } from '../responses';
+import { LoggersService } from 'src/loggers/loggers.service';
 export class ErrorResponse extends Error {
    public message: string;
    public metadata: any;
    public statusCode: number;
+   public logger: LoggersService;
    constructor({ message, statusCode, metadata = {} }: MessageResponse) {
       super();
       this.message = message;
@@ -12,6 +14,7 @@ export class ErrorResponse extends Error {
    }
 
    send(res: Response, headers: Record<string, string> = {}): Response {
+      this.logger.warn(this.message, this.metadata);
       Object.keys(headers).forEach((key) => {
          res.header(key, headers[key]);
       });
