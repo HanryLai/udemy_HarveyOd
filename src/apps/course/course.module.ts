0a51a8a1a0adcd19ecf1c0e724c26682bc3910a0
hieulai <ldmhieudev@gmail.com>
2024-07-31 23:32:32 +0700

    Feat: Import TagModule to CourseModule

--- src/apps/course/course.module.ts
+++ src/apps/course/course.module.ts
@@ -6,11 +6,13 @@ import { CategoryEntity, CourseEntity } from 'src/entities/courses';
 import { KeyTokenEntity } from 'src/entities/auth';
 import { RedisService } from 'src/common/redis/redis.service';
 import { CategoryModule } from '../category/category.module';
+import { TagModule } from '../tag/tag.module';
 
 @Module({
    imports: [
       TypeOrmModule.forFeature([CourseEntity, KeyTokenEntity, CategoryEntity]),
       forwardRef(() => CategoryModule),
+      forwardRef(() => TagModule),
    ],
    controllers: [CourseController],
    providers: [CourseService, RedisService],
